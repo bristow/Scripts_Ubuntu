1f5ebf940934dadf07732b80a5c507439a5623a3
Simon B <10208905+simbd@users.noreply.github.com>
2018-03-13 09:26:52 +0100

    Create Celestia_pour_Bionic.sh

---
+++ Celestia_pour_Bionic.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Script à lancer avec sudo !
+# Ce script sert à automatiser l'installation du logiciel d'astronomie "Celestia" pour Ubuntu 18.04 Bionic Beaver X64
+
+# Testé/Validé pour Ubuntu 18.04LTS Bionic Beaver X64 avec Gnome Shell 
+
+# Récupération dépendance manquante 
+wget http://mirrors.kernel.org/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1_amd64.deb
+#lien alternatif si ne fonctionne plus : http://nux87.free.fr/script-postinstall-ubuntu/deb/libpng12amd64.deb
+dpkg -i libpng12-0_1.2.54-1ubuntu1_amd64.deb
+
+# Autres paquets nécessaires
+apt install liblua5.1-0 freeglut3 libgtkglext1 libgnome2-0 libgnomeui-0 -y
+
+#Récup célestia
+wget http://nux87.free.fr/script-postinstall-ubuntu/deb/celestia-common1.6.1all.deb
+wget http://nux87.free.fr/script-postinstall-ubuntu/deb/celestia-gnome1.6.1amd64.deb
+dpkg -i celestia*
+apt install -fy
+
+#nettoyage
+rm libpng12-0_1.2.54-1ubuntu1_amd64.deb celestia*
+clear
