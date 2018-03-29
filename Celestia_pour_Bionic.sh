4e5abd178f135784a091c5f5442ce149cbbe3938
Simbd <10208905+simbd@users.noreply.github.com>
2018-03-29 16:03:59 +0200

    Update Celestia_pour_Bionic.sh

--- Celestia_pour_Bionic.sh
+++ Celestia_pour_Bionic.sh
@@ -5,9 +5,9 @@
 # Testé/Validé pour Ubuntu 18.04LTS Bionic Beaver X64 avec Gnome Shell 
 
 # Récupération dépendance manquante 
-wget http://mirrors.kernel.org/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1_amd64.deb
-#lien alternatif si ne fonctionne plus : http://nux87.free.fr/script-postinstall-ubuntu/deb/libpng12amd64.deb
-dpkg -i libpng12-0_1.2.54-1ubuntu1_amd64.deb
+#wget http://mirrors.kernel.org/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1_amd64.deb  ## pb avec le mirroir
+wget http://nux87.free.fr/script-postinstall-ubuntu/deb/libpng12amd64.deb
+dpkg -i libpng12*
 
 # Autres paquets nécessaires
 apt install liblua5.1-0 freeglut3 libgtkglext1 libgnome2-0 libgnomeui-0 -y
